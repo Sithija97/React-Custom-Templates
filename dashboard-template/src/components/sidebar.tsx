be26974d2ef3f79361751b749afbea88670d8d75
Sithija Shehara <nsithijashehara@gmail.com>
2024-01-04 22:13:42 +0530

    siderbar UI

--- dashboard-template/src/components/sidebar.tsx
+++ dashboard-template/src/components/sidebar.tsx
@@ -1,4 +1,8 @@
 import { ReactNode } from "react";
+// import { Link } from "react-router-dom";
+import { RxSketchLogo, RxDashboard, RxPerson } from "react-icons/rx";
+import { HiOutlineShoppingBag } from "react-icons/hi";
+import { FiSettings } from "react-icons/fi";
 
 type SidebarProps = {
   children: ReactNode;
@@ -7,7 +11,28 @@ type SidebarProps = {
 export const Sidebar = ({ children }: SidebarProps) => {
   return (
     <div className="flex">
-      <div className="fixed w-20 h-screen p-4 bg-white border-r-[1px] flex flex-col justify-between"></div>
+      <div className="fixed w-20 h-screen p-4 bg-white border-r-[1px] flex flex-col justify-between">
+        <div className="flex flex-col items-center">
+          {/* <Link to="/"> */}
+          <div className="bg-purple-800 text-white p-3 rounded-lg inline-block">
+            <RxSketchLogo size={20} />
+          </div>
+          {/* </Link> */}
+          <span className="border-b-[1px] border-gra w-full p-2"></span>
+          <div className="bg-gray-100 hover:bg-gray-200 cursor-pointer my-4  p-3 rounded-lg inline-block">
+            <RxDashboard size={20} />
+          </div>
+          <div className="bg-gray-100 hover:bg-gray-200 cursor-pointer my-4  p-3 rounded-lg inline-block">
+            <RxPerson size={20} />
+          </div>
+          <div className="bg-gray-100 hover:bg-gray-200 cursor-pointer my-4  p-3 rounded-lg inline-block">
+            <HiOutlineShoppingBag size={20} />
+          </div>
+          <div className="bg-gray-100 hover:bg-gray-200 cursor-pointer my-4  p-3 rounded-lg inline-block">
+            <FiSettings size={20} />
+          </div>
+        </div>
+      </div>
       <main className="ml-20 w-full">{children}</main>
     </div>
   );
